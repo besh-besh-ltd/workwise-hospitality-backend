8d361b68b7589108d007bdbcfa5a70b8b8029dd1
aaditya kumar jha <aaditya@letsworkwise.com>
2024-08-12 16:16:53 +0530

    subscription validation implemented

--- app/controllers/rfq/rfqController.js
+++ app/controllers/rfq/rfqController.js
@@ -2122,7 +2122,7 @@ const rfqController = {
            .then((result) => {
              shuffleArray(result);
              Array.apply(null, { length: rest_items }).map((item) => {
-               result.push(dummyOBJ);
+               result.push({total:total_items});
              });
 
              res
