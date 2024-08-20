4f64aab5f27bfdb2731c0a158561fa204740d540
mukulsomukesh <mukulsomukesh@gmail.com>
2024-08-20 14:17:39 +0530

    changes "review-buyers-private-vendor" POST to PUT request

--- app/routes/admin/buyerRoutes.js
+++ app/routes/admin/buyerRoutes.js
@@ -73,7 +73,7 @@ buyerRoutes.delete(
 );
 
 // in this route we will do final review of buyer private vendors and move them temp_user_table to to user_table, and delete data from current table
-buyerRoutes.post(
+buyerRoutes.put(
   '/review-buyers-private-vendor',
   passportSignIn,
   buyerController.reviewBuyerPrivateVendors
