4b3945e75afb7e526a010116b1742f31d654ec1c
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2024-09-04 15:02:13 +0530

    Fix: GetTourProgress API fixed

--- app/models/portalTourModel.js
+++ app/models/portalTourModel.js
@@ -45,19 +45,23 @@ const portalTourModel = {
     },
     // get user tour progress by page ID and user ID
     getUserTourStatus: async (page_id, user_id) => {
-        return new Promise((resolve, reject) => {
-            db.one(
-                `SELECT * FROM tbl_portal_tour_progress WHERE page_id=$1 AND user_id=$2`,
+        try {
+            // Use db.oneOrNone to handle cases where no data is found
+            const data = await db.oneOrNone(
+                `SELECT * FROM tbl_portal_tour_progress WHERE page_id=$1 AND user_id=$2 AND completed=true`,
                 [page_id, user_id]
-            )
-                .then(function (data) {
-                    resolve(data);
-                })
-                .catch(function (err) {
-                    let error = new Error(err);
-                    reject(error);
-                });
-        })
+            );
+
+            if (data) {
+                return { tourProgress: 1 };
+            } else {
+                return { tourProgress: 0 };
+            }
+        } catch (err) {
+            // Handle and reject errors
+            const error = new Error(err);
+            throw error;
+        }
     },
     //  upload user tour progress
     uploadUserProgress: async (user_id, completed, page_id) => {
