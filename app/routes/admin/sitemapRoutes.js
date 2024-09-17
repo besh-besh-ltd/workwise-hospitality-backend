0c3fe26598009704e7dbdc9f289c08c8526400ad
mukulsomukesh <mukulsomukesh@gmail.com>
2024-09-17 12:29:58 +0530

    chore: remove sitemap APIs from development environment

--- app/routes/admin/sitemapRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import sitemapController from '../../controllers/admin/sitemapController.js';
-
-const sitemapRoutes = Router();
-
-sitemapRoutes.get('/vendor-list', sitemapController.vendorProfiles);
-
-export default sitemapRoutes;
