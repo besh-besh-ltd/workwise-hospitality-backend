6bc6aa9623b1e4a928c76927942eccd0b7472ba6
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-02 16:50:35 +0530

    fix: return product slug only if we have vendor for that product

--- app/models/seoModel.js
+++ app/models/seoModel.js
@@ -3,7 +3,7 @@ import db from '../config/dbConn.js';
 const seoModel = {
     productSlugSitemap: async () => {
       return new Promise(function (resolve, reject) {
-        db.any('select DISTINCT slug from tbl_product')
+        db.any('select DISTINCT slug from tbl_product  WHERE created_by NOT IN (1, 111)')
           .then(function (data) {
             resolve(data);
           })
