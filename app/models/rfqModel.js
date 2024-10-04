0bfd0615294f45b6d9e998b6bb2ba57969dc60a8
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-04 12:53:45 +0530

    fix: /getMyRfq updated sending quote status, pending, sent, regret

--- app/models/rfqModel.js
+++ app/models/rfqModel.js
@@ -238,12 +238,18 @@ const rfqModel = {
                 JOIN tbl_rfq_product_vendors trpv ON trpv.rfq_id = RFQ.id AND trpv.user_id = ${user_id} AND trpv.product_id = RFQ_P.product_id
                 WHERE RFQ.id = RFQ_P.rfq_id AND trpv.rfq_id = RFQ.id AND trpv.user_id = ${user_id} AND trpv.product_id = RFQ_P.product_id
             ) AS "products" ,
-           CASE WHEN (SELECT U.user_type FROM tbl_users U WHERE U.id = ${user_id}) = 3 THEN (
-                EXISTS (
-                    SELECT 1 FROM tbl_quotes TQ
-                    WHERE TQ.rfq_id = RFQ.id AND TQ.rfq_no = RFQ.rfq_no AND TQ.created_by = ${user_id}
-                )
-            ) ELSE false END AS "quote_sent"
+            CASE 
+                WHEN EXISTS (
+                SELECT * FROM tbl_quotes TQ
+                WHERE TQ.rfq_id = RFQ.id AND TQ.rfq_no = RFQ.rfq_no AND TQ.created_by = ${user_id}
+             ) THEN
+             CASE 
+             WHEN (SELECT TQ.is_regret FROM tbl_quotes TQ 
+                  WHERE TQ.rfq_id = RFQ.id AND TQ.rfq_no = RFQ.rfq_no AND TQ.created_by = ${user_id} LIMIT 1) = 1 THEN 'regret'
+             ELSE 'sent'
+            END
+            ELSE 'pending'
+        END AS "quote_status"
         FROM tbl_rfq RFQ
         WHERE EXISTS (
             SELECT 1
