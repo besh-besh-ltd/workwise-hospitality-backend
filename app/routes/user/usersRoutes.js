eae04260a51138f6321bba2256d1baa24a297cd7
yashTheYcg <yashchandil20@gmail.com>
2024-12-03 17:44:56 +0530

    fix: add some comments above upload-file endpoint and commented the upload-file-without-auth endpoint

--- app/routes/user/usersRoutes.js
+++ app/routes/user/usersRoutes.js
@@ -111,6 +111,11 @@ UsersRoutes.get(
   // passportSignIn,
   UsersController.vendorapprove_list
 );
+
+// this needs to be change in future.....
+// in no authorization token we are takng the token from the query
+// for getting the user_id of the user when it is valid then only 
+// we upload the file.
 UsersRoutes.post(
   '/upload-file',
   noLogin.customer_auth,
@@ -119,12 +124,12 @@ UsersRoutes.post(
 );
 // Endpoint for uploading the file without authentication
 // This can be dangerous but let's see in future .
-UsersRoutes.post(
-  '/upload-file-without-auth',
-  noLogin.customer_auth,
-  schema_posts.upload_document_without_auth,
-  UsersController.upload_document_without_auth
-);
+// UsersRoutes.post(
+//   '/upload-file-without-auth',
+//   noLogin.customer_auth,
+//   schema_posts.upload_document_without_auth,
+//   UsersController.upload_document_without_auth
+// );
 UsersRoutes.get(
   '/vendor-profile/:vendor_id',
   noLogin.customer_auth,
