98e5685645f5f6135568d8fec67973d7a867a6a3
SK Imtiaj Uddin <imtiaj@letsworkwise.com>
2024-12-13 16:51:48 +0530

    feature: Implemented quotes and cost analysis API.

--- app/models/rfqModel.js
+++ app/models/rfqModel.js
@@ -2130,6 +2130,72 @@ WHERE row_num_by_name_category = 1
       throw new Error(error);
     }
   },
+  getQuotesChartData: async (user_id, chartFilter, start_date, end_date) => {
+    const dateQ = ['past7days', 'currentMonth'].includes(chartFilter)
+    const query = `
+      SELECT 
+          ${dateQ 
+              ? `DATE(tqf.timestamp) AS date,`
+              : `TO_CHAR(tqf.timestamp, 'YYYY-MM') AS month,`}
+          count(tqf.id) AS quotes_count 
+      FROM tbl_quote_finalization tqf
+      JOIN tbl_rfq tr
+          ON tr.id = tqf.rfq_id
+      WHERE tqf.timestamp BETWEEN $3::timestamp AND $4::timestamp
+        AND tr.created_by = $1 
+      ${dateQ 
+          ? `GROUP BY DATE(tqf.timestamp)
+            ORDER BY date;`
+          : `GROUP BY TO_CHAR(tqf.timestamp, 'YYYY-MM')
+            ORDER BY month;`}`;
+
+    console.log(query)
+    try {
+      const formattedStartDate = new Date(start_date).toISOString();
+      const formattedEndDate = new Date(end_date).toISOString();
+      const values = [user_id, 1, formattedStartDate, formattedEndDate];
+
+      const result = await db.query(query, values);
+      return result;
+    } catch (error) {
+      console.log(error)
+      throw new Error(error);
+    }
+  },
+  getQuoteCostingData: async (user_id, chartFilter, start_date, end_date) => {
+    const dateQ = ['past7days', 'currentMonth'].includes(chartFilter)
+    const query = `
+      SELECT
+          ${dateQ
+              ? `DATE(tqf.timestamp) AS date,`
+              : `TO_CHAR(tqf.timestamp, 'YYYY-MM') AS month,`}
+          SUM(tqi.total_price) AS total_quote_amount
+      FROM tbl_quote_finalization tqf
+      JOIN tbl_quote_items tqi
+          ON tqf.id = tqi.quote_id
+      JOIN tbl_rfq tr
+          ON tr.id = tqf.rfq_id
+      WHERE tqf.timestamp BETWEEN $3::timestamp AND $4::timestamp
+        AND tr.created_by = $1
+      ${dateQ
+          ? `GROUP BY DATE(tqf.timestamp)
+            ORDER BY date;`
+          : `GROUP BY TO_CHAR(tqf.timestamp, 'YYYY-MM')
+            ORDER BY month;`}
+      `;
+
+    try {
+      const formattedStartDate = new Date(start_date).toISOString();
+      const formattedEndDate = new Date(end_date).toISOString();
+      const values = [user_id, 1, formattedStartDate, formattedEndDate];
+
+      const result = await db.query(query, values);
+      return result;
+    } catch (error) {
+      console.log(error)
+      throw new Error(error);
+    }
+  },
   getAllRfqByUser: async (user_id, status) => {
     const query = `
       SELECT count(*) 
@@ -2215,7 +2281,7 @@ WHERE row_num_by_name_category = 1
     } catch (error) {
       throw new Error(error);
     }    
-  },
+  },  
   getVendorReviews: async (vendorId) => {
     return new Promise(function (resolve, reject) {
       db.query(
