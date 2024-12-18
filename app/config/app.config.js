9d701cabe123b814fba58f7605176e6264455487
mukulsomukesh <mukulsomukesh@gmail.com>
2024-12-18 10:41:45 +0530

    chore: included dev team email in config and moved smtp cred in .env file

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -36,8 +36,8 @@ const invoiceFileUploadPath = process.env.INVOICE_FILE || null;
 // const blogImageUploadPath = process.env.BLOG_IMAGE || null;
 
 const downloadURL = process.env.DOWNLOADD_URL || null;
-const jwtSecret = process.env.JWT_SECRET || "";
-const cryptSecret = process.env.CRYPT_SECRET || "";
+const jwtSecret = process.env.JWT_SECRET || "sfs64SG4wg";
+const cryptSecret = process.env.CRYPT_SECRET || "Anha%43wsfse";
 const globalAdminLimit = process.env.LIMIT || 20;
 const sgst = process.env.SGST || 0.0015;
 const cgst = process.env.CGST || 0.0015;
@@ -301,19 +301,23 @@ const config = {
     host: "smtp-relay.brevo.com",
     port: 587,
     auth: {
-      user: "b2bportal2023@gmail.com",
-      pass: "xsmtpsib-3b42f9886e4f809b920bfec2a77b5daa71c65fdcdf3e71d0c247d25261f4446e-J7znQEFTHW45M9Sw",
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_USER_PASSWORD,
     },
   },
   developers: [
-    "Sourav Maity <sourav.maity@indusnet.co.in>",
-    "Abhisek Pal <abhisek.pal@indusnet.co.in>",
+    "Gyan <gyan@letsworkwise.com>",
+    "Yash <yash@letsworkwise.com>",
+    "Imtiaj <imtiaj@letsworkwise.com >",
+    // "Sourav Maity <sourav.maity@indusnet.co.in>",
+    // "Abhisek Pal <abhisek.pal@indusnet.co.in>",
   ],
   admin_email: [
-    "sourav.maity@indusnet.co.in",
-    "abhisek.pal@indusnet.co.in",
-    "ranit.majumder@indusnet.co.in",
-    "anumita.banerjee@indusnet.co.in",
+    'mukul@letsworkwise.com',
+    // 'sourav.maity@indusnet.co.in',
+    // 'abhisek.pal@indusnet.co.in',
+    // 'ranit.majumder@indusnet.co.in',
+    // 'anumita.banerjee@indusnet.co.in'
   ],
   webmasterMail: "Work Wise <hello@letsworkwise.com>",
   template_path:
