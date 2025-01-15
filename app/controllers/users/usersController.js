38f9924524adb2bb5ed797b53a10d203c8883a4a
mukulsomukesh <mukulsomukesh@gmail.com>
2025-01-15 14:57:35 +0530

    fix: category mapping with product, buyer vendor management part

--- app/controllers/users/usersController.js
+++ app/controllers/users/usersController.js
@@ -2960,7 +2960,7 @@ const UsersController = {
     }
   },
 
-  addApprovedPrivateVendor: async (req, res, next) => {
+  addApprovedPrivateVendor: async (req, res) => {
 
     try {
 
@@ -3189,7 +3189,7 @@ const UsersController = {
               category_id: categoryId,
               product_id: productId
             };
-            await productModel.createProductCategories(categoryObj);
+            await productModel.createProductCategory(categoryObj);
           }
         }
 
