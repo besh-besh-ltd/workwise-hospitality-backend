3fe15f919108828dda3cd95ba3dcfd2f82095d49
ayush-workwise <ayush@letsworkwise.com>
2025-02-22 10:50:34 +0530

    Add Get Country,State,and City

--- app/controllers/general/generalController.js
+++ app/controllers/general/generalController.js
@@ -5,25 +5,29 @@ import generalModel from '../../models/generalModel.js';
 const generalController = {
   getStates: async (req, res, next) => {
     try {
-      const states = await generalModel.getStates();
-      res
-        .status(200)
-        .json({
-          status: 1,
-          data: states
-        })
-        .end();
+      const country_id = req.query.country_id;
+  
+      let states;
+      if (country_id) {
+        // Convert country_id to integer (optional, based on your DB setup)
+        states = await generalModel.getCountryStates(country_id);
+      } else {
+        states = await generalModel.getStates();
+      }
+  
+      res.status(200).json({
+        status: 1,
+        data: states
+      }).end();
     } catch (error) {
       logError(error);
-      res
-        .status(400)
-        .json({
-          status: 3,
-          message: Config.errorText.value
-        })
-        .end();
+      res.status(400).json({
+        status: 3,
+        message: Config.errorText.value
+      }).end();
     }
-  },
+  }
+  ,
   getCities: async (req, res, next) => {
     const state_id = req.params.id;
     try {
