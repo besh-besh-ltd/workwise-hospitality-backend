668fa673162328ef639ddd33b5c075aa81363481
mukulsomukesh <mukulsomukesh@gmail.com>
2025-06-05 22:51:10 +0530

    Fix: Ensure processed URL uses HTTPS in UAT environment

--- app/controllers/rfq/rfqController.js
+++ app/controllers/rfq/rfqController.js
@@ -5819,9 +5819,7 @@ const rfqController = {
         }
       }
 
-      if (process.env.NODE_ENV=='uat' &&  processedUrl.startsWith('http:')) {
         processedUrl = processedUrl.replace('http:', 'https:');
-      }
   
       const boqDataJson = await generativeAI.processBOQWithAI(processedUrl);
 
