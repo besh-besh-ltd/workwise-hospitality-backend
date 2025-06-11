aea30e4ad248ef148945a80a2c5219415422504d
mukulsomukesh <mukulsomukesh@gmail.com>
2025-06-11 19:44:00 +0530

    Refactor update_user_detail method: streamline response handling and improve error logging

--- app/controllers/users/usersController.js
+++ app/controllers/users/usersController.js
@@ -1247,21 +1247,28 @@ update_user_detail: async (req, res, next) => {
         updateData,
         `id = ${targetUserId} AND company_id = ${loggedInUser.company_id}`
     );
-    } else {
+    } else { // we can use updateWhere insted of updateUserAccount
       result = await userModel.updateUserAccount(targetUserId, updateData);
     }
 
-    return res.status(200).json({
-      status: 1,
-      message: "User profile updated successfully"
-    });
-  } catch (err) {
-    logError(err);
-    return res.status(400).json({
-      status: false,
-      message: Config.errorText.value
-    });
-  }
+
+        res
+          .status(200)
+          .json({
+            status: 1,
+            message: 'User profile updated successfully'
+          })
+          .end();
+  } catch (error) {
+    logError(error);
+      res
+        .status(400)
+        .json({
+          status: 3,
+          message: Config.errorText.value
+        })
+        .end();
+    }
 },
 
 
