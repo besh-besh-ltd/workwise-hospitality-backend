a9109438f14fd66f9e8e6b3ea8fbb7f12791121e
KushalwithK <kushalshah821@gmail.com>
2025-07-03 16:47:00 +0530

    Revert: newrelic.cjs file and .gitignore line

---
+++ newrelic.cjs
@@ -0,0 +1,13 @@
+'use strict'
+
+exports.config = {
+  app_name: ['Workwise Backend staging'],
+  license_key: '_test_ae156cb0165d2bfbff1716f6667c1e60FFFFNRAL',
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    enabled: true
+  }
+}
