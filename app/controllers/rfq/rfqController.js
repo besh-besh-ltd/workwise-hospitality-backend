233841130214e43e4b866efa2bfa0e53afd40ab4
KushalwithK <kushalshah821@gmail.com>
2025-07-30 19:17:30 +0530

    Fix: Similar products will also be added in vendor error and validation errors will not disappear once product is selected

--- app/controllers/rfq/rfqController.js
+++ app/controllers/rfq/rfqController.js
@@ -3329,17 +3329,17 @@ deleteDraft: async (req, res) => {
           }
         }
 
-        if(product.isUnfoundProduct && product.unfoundName) {
-          let validationErrors = sheetData.validation_errors;
-          if(validationErrors && Array.isArray(validationErrors) && validationErrors.length > 0) {
-            const updatedErrors = validationErrors.filter(error => error.name != product.unfoundName);
+        // if(product.isUnfoundProduct && product.unfoundName) {
+        //   let validationErrors = sheetData.validation_errors;
+        //   if(validationErrors && Array.isArray(validationErrors) && validationErrors.length > 0) {
+        //     const updatedErrors = validationErrors.filter(error => error.name != product.unfoundName);
 
-            const updatedSheetData = {
-              validation_errors: JSON.stringify(updatedErrors)
-            }
-            await rfqModel.update('tbl_rfq_draft_sheets', updatedSheetData, sheet_id);
-          }
-        }
+        //     const updatedSheetData = {
+        //       validation_errors: JSON.stringify(updatedErrors)
+        //     }
+        //     await rfqModel.update('tbl_rfq_draft_sheets', updatedSheetData, sheet_id);
+        //   }
+        // }
 
         res.status(200).json({
             status: 1,
@@ -6931,6 +6931,7 @@ deleteDraft: async (req, res) => {
             unit: item.unit || '',
             sheet_name: item.sheet_name || '',
             description: item.full_product_description || '',
+            similar_products: item.reranked_variants || [],
           });
           continue;
         }
