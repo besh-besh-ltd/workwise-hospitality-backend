2164abba67612df47d419b20aa5c2f30e801115a
kushaldevs <kushal@letsworkwise.com>
2025-10-30 13:11:12 +0530

    Migrated: Some functions to use AISensy instead of FluxChat

--- app/helper/whatsappNotificationFluxChat.js
+++ app/helper/whatsappNotificationFluxChat.js
@@ -4,7 +4,8 @@ import axios from 'axios'; // Import axios for making HTTP requests
 const flux_chat_api = process.env.FLUX_CHAT_API
 const flux_chat_bearer_token =  process.env.FLUX_CHAT_KEY
 
-
+const aisensy_api = process.env.AISENSY_API
+const aisensy_bearer_token =  process.env.AISENSY_API_KEY
 
 
 const formatPhoneNumber = (input) => {
@@ -29,49 +30,34 @@ const whatsappNotificationFluxChat = {
 buyerCreatesRFQNotification: async (payload) => {
   // Construct the data payload for the WhatsApp message
   const data = {
-    messaging_product: "whatsapp",
-    recipient_type: "individual",
-    to: formatPhoneNumber(payload.mobile), // Assuming you have a function to format the phone number
-    type: "template",
-    template: {
-      name: "buyer_creates_rfq",
-      language: {
-        policy: "deterministic",
-        code: "en"
-      },
-      components: [
-        {
-          type: "body",
-          parameters: [
-            {
-              type: "text",
-              text: `${payload.rfq_no}`
-            }
-          ]
-        },
-        {
-          type: "button",
-          sub_type: "url",
-          index: 0,
-          parameters: [
-            {
-              type: "text",
-              text: `dashboard/buyer/rfq-management-details?type=buyer-view&id=${payload.rfq_id}` // Assuming you're passing RFQ ID to generate a URL
-            }
-          ]
-        }
-      ]
+    apiKey: aisensy_bearer_token,
+    campaignName: 'buyerCreatesRFQNotification',
+    destination: formatPhoneNumber(payload.mobile),
+    userName: 'letsworkwise',
+    templateParams: [
+      `${payload.rfq_no}`,
+      `dashboard/buyer/rfq-management-details?type=buyer-view&id=${payload.rfq_id}`
+    ],
+    source: 'Workwise Platform',
+    media: {},
+    buttons: [],
+    carouselCards: [],
+    location: {},
+    attributes: {},
+    paramsFallbackValue: {
+      FirstName: 'user'
     }
   };
 
   // Headers for the API request
+  
   const headers = {
     'Content-Type': 'application/json',
-    Authorization: flux_chat_bearer_token // Replace with your actual API key for the messaging service
+    // Authorization: flux_chat_bearer_token // Replace with your actual API key for the messaging service
   };
 
   // Make the POST request to the messaging API
-  await axios.post(flux_chat_api, data, { headers: headers })
+  await axios.post(aisensy_api, data, { headers: headers })
   .then(response => {
       console.log('RFQ creation notification sent:');
     })
@@ -85,55 +71,32 @@ buyerCreatesRFQNotification: async (payload) => {
 vendorReceivesRFQNotification: async (payload) => {
   // Construct the data payload for the WhatsApp message
   const data = {
-    messaging_product: "whatsapp",
-    recipient_type: "individual",
-    to: formatPhoneNumber(payload.mobile), // Ensure to format the vendor's phone number
-    type: "template",
-    template: {
-      name: "vendor_receives_rfq_from_buyer",
-      language: {
-        policy: "deterministic",
-        code: "en_US"
-      },
-      components: [
-        {
-          type: "body",
-          parameters: [
-            {
-              type: "text",
-              text: payload.vendorName // Example detail - RFQ number
-            },
-            {
-              type: "text",
-              text: payload.buyerName // Example detail - Buyer name
-            },
-            {
-              type: "text",
-              text: payload.productDetails // Example detail - Project name
-            }
-          ]
-        },
-        {
-          type: "button",
-          sub_type: "url",
-          index: 0,
-          parameters: [
-            {
-              type: "text",
-              text: `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}` // Direct URL to RFQ details
-            }
-          ]
-        }
-      ]
+    apiKey: aisensy_bearer_token,
+    campaignName: 'VendorRecievesRFQFromBuyer',
+    destination: formatPhoneNumber(payload.mobile),
+    userName: 'letsworkwise',
+    templateParams: [
+      `${payload.vendorName}`,
+      `${payload.buyerName}`,
+      `${payload.productDetails}`,
+      `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
+    ],
+    source: 'Workwise Platform',
+    media: {},
+    buttons: [],
+    carouselCards: [],
+    location: {},
+    attributes: {},
+    paramsFallbackValue: {
+      FirstName: 'user'
     }
-  }
+  };
   // Headers for the API request
   const headers = {
     'Content-Type': 'application/json',
-    Authorization: flux_chat_bearer_token // Replace with your actual API key for the messaging service
   };
   // Make the POST request to the messaging API
-  await axios.post(flux_chat_api, data, { headers: headers })
+  await axios.post(aisensy_api, data, { headers: headers })
   .then(response => {
       console.log('RFQ received notification sent to vendor:', response.data);
     })
@@ -147,62 +110,34 @@ vendorReceivesRFQNotification: async (payload) => {
 
 
 sendQuoteSubmissionNotification: async (payload) =>{
-const data = {
-  "messaging_product": "whatsapp",
-  "recipient_type": "individual",
-  "to": formatPhoneNumber(payload.mobile),
-  "type": "template",
-  "template": {
-    "name": "vendor_submit_quote_to_buyer_temp_vendor",
-    "language": {
-      "policy": "deterministic",
-      "code": "en_US"
-    },
-    "components": [
-      {
-        "type": "header",
-        "parameters": [
-          {
-            "type": "text",
-            "text": "Submitted"
-          }
-        ]
-      },
-      {
-        "type": "body",
-        "parameters": [
-          {
-            "type": "text",
-            "text": payload.name
-          },
-          {
-            "type": "text",
-            "text": payload.message
-          }
-        ]
-      },
-      {
-        "type": "button",
-        "sub_type": "url",
-        "index": 0,
-        "parameters": [
-          {
-            "type": "text",
-            "text": `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
-          }
-        ]
-      }
-    ]
-  }
-}
+
+  const data = {
+    apiKey: aisensy_bearer_token,
+    campaignName: 'VendorSubmitQuote',
+    destination: formatPhoneNumber(payload.mobile),
+    userName: 'letsworkwise',
+    templateParams: [
+      `${payload.name}`,
+      `${payload.message}`,
+      `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
+    ],
+    source: 'new-landing-page form',
+    media: {},
+    buttons: [],
+    carouselCards: [],
+    location: {},
+    attributes: {},
+    paramsFallbackValue: {
+      FirstName: 'user'
+    }
+  };
 
     const headers = {
       'Content-Type': 'application/json',
-      Authorization: flux_chat_bearer_token // Replace with your Flux API key
     };
   
    await axios
-      .post(flux_chat_api, data, { headers: headers })
+      .post(aisensy_api, data, { headers: headers })
       .then((response) => {
         console.log('WhatsApp Message Sent:', response.data);
       })
@@ -216,36 +151,27 @@ const data = {
 
   contactUsFormWhatsAppMessage: async (payload) => {
     const data = {
-      messaging_product: 'whatsapp',
-      recipient_type: 'individual',
-      to: formatPhoneNumber(payload.phone) , // Replace with the recipient's phone number
-      type: 'template',
-      template: {
-        name: 'contactus_form_submit_by_users',
-        language: {
-          policy: 'deterministic',
-          code: 'en_US'
-        },
-        components: [
-          {
-            type: 'body',
-            parameters: [
-              {
-                type: 'text',
-                text: payload.name
-              }
-            ]
-          }
-        ]
+      apiKey: aisensy_bearer_token,
+      campaignName: 'ContactFormSubmit',
+      destination: formatPhoneNumber(payload.phone),
+      userName: 'letsworkwise',
+      templateParams: [`${payload.name}`],
+      source: 'new-landing-page form',
+      media: {},
+      buttons: [],
+      carouselCards: [],
+      location: {},
+      attributes: {},
+      paramsFallbackValue: {
+        FirstName: 'user'
       }
     };
     const headers = {
       'Content-Type': 'application/json',
-      Authorization: flux_chat_bearer_token // Replace with your Flux API key
     };
   
    await axios
-      .post(flux_chat_api, data, { headers: headers })
+      .post(aisensy_api, data, { headers: headers })
       .then((response) => {
         console.log('WhatsApp Message Sent:', response.data);
       })
@@ -258,62 +184,84 @@ const data = {
   },
 
   sendQuoteReminderNotificationToVendor: async (payload) => {
+
+    const data = {
+      apiKey: aisensy_bearer_token,
+      campaignName: 'SendReminderNotification',
+      destination: formatPhoneNumber(payload.mobile),
+      userName: 'letsworkwise',
+      templateParams: [
+        `${payload.name}`,
+        `${payload.buyerName}`,
+        `${payload.rfq_no}`,
+        `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
+      ],
+      source: 'Workwise Platform',
+      media: {},
+      buttons: [],
+      carouselCards: [],
+      location: {},
+      attributes: {},
+      paramsFallbackValue: {
+        FirstName: 'user'
+      }
+    };
   
     // Construct the data payload
-    const data = {
-      "messaging_product": "whatsapp",
-      "recipient_type": "individual",
-      "to": formatPhoneNumber(payload.mobile),
-      "type": "template",
-      "template": {
-        "name": "rfq_quote_reminder_temp_vendor",
-        "language": {
-          "policy": "deterministic",
-          "code": "en_US"
-        },
-        "components": [
-          {
-            "type": "body",
-            "parameters": [
-              {
-                "type": "text",
-                "text": payload.name
-              },
-              {
-                "type": "text",
-                "text": payload.buyerName
-              },
-              {
-                "type": "text",
-                "text": payload.rfq_no
-              }
-            ]
-          },
-          {
-            "type": "button",
-            "sub_type": "url",
-            "index": 0,
-            "parameters": [
-              {
-                "type": "text",
-                "text": `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
-              }
-            ]
-          }
-        ]
-      }
-    }
+    // const data = {
+    //   "messaging_product": "whatsapp",
+    //   "recipient_type": "individual",
+    //   "to": formatPhoneNumber(payload.mobile),
+    //   "type": "template",
+    //   "template": {
+    //     "name": "rfq_quote_reminder_temp_vendor",
+    //     "language": {
+    //       "policy": "deterministic",
+    //       "code": "en_US"
+    //     },
+    //     "components": [
+    //       {
+    //         "type": "body",
+    //         "parameters": [
+    //           {
+    //             "type": "text",
+    //             "text": payload.name
+    //           },
+    //           {
+    //             "type": "text",
+    //             "text": payload.buyerName
+    //           },
+    //           {
+    //             "type": "text",
+    //             "text": payload.rfq_no
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         "type": "button",
+    //         "sub_type": "url",
+    //         "index": 0,
+    //         "parameters": [
+    //           {
+    //             "type": "text",
+    //             "text": `dashboard/vendor/inquiries-details?id=${payload.rfq_id}&token=${payload.token}`
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   }
+    // }
   
     // 3) Set your request headers (include your Flux API key)
     const headers = {
       'Content-Type': 'application/json',
-      Authorization: flux_chat_bearer_token // Replace with your Flux API key
+      // Authorization: flux_chat_bearer_token // Replace with your Flux API key
     };
   
     console.log(data)
 
     // 4) Make the POST request
-    await axios.post(flux_chat_api, data, { headers: headers })
+    await axios.post(aisensy_api, data, { headers: headers })
     .then(response => {
         console.log('New Quote Notification Sent:', response.data);
       })
@@ -329,73 +277,36 @@ const data = {
   
     // Construct the data payload
     const data = {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      to: formatPhoneNumber(payload.mobile) , 
-      type: "template",
-      template: {
-        name: "buyer_new_quote_receive",
-        language: {
-          policy: "deterministic",
-          code: "en"
-        },
-        components: [
-          {
-            type: "header",
-            parameters: [
-              {
-                type: "text",
-                text: payload.rfqNumber // e.g., the RFQ number
-              }
-            ]
-          },
-          // Body - The template has 4 placeholders for body text
-          {
-            type: "body",
-            parameters: [
-              {
-                type: "text",
-                text: payload?.buyerName // 1st placeholder, e.g., buyer name
-              },
-              {
-                type: "text",
-                text: payload?.vendorName // 2nd placeholder, e.g., vendor name
-              },
-              {
-                type: "text",
-                text: payload?.projectName || "" // 3rd placeholder, e.g., project name
-              },
-              {
-                type: "text",
-                text: payload.rfqNumber // 4th placeholder, e.g., other details
-              }
-            ]
-          },
-          {
-            type: "button",
-            sub_type: "url",
-            index: 0,
-            parameters: [
-              {
-                type: "text",
-                text: `dashboard/buyer/quote-compare?rfq=${payload.rfqID}`
-              }
-            ]
-          }
-        ]
+      apiKey: aisensy_bearer_token,
+      campaignName: 'BuyerNewQuoteReceived',
+      destination: formatPhoneNumber(payload.mobile),
+      userName: 'letsworkwise',
+      templateParams: [
+        `${payload.rfqNumber}`
+        `${payload?.buyerName}`,
+        `${payload?.vendorName}`,
+        `${payload?.projectName}`,
+        `${payload.rfqNumber}`,
+        `dashboard/buyer/quote-compare?rfq=${payload.rfqID}`
+      ],
+      source: 'new-landing-page form',
+      media: {},
+      buttons: [],
+      carouselCards: [],
+      location: {},
+      attributes: {},
+      paramsFallbackValue: {
+        FirstName: 'user'
       }
     };
   
     // 3) Set your request headers (include your Flux API key)
     const headers = {
       'Content-Type': 'application/json',
-      Authorization: flux_chat_bearer_token // Replace with your Flux API key
     };
-  
-    console.log(data)
 
     // 4) Make the POST request
-    await axios.post(flux_chat_api, data, { headers: headers })
+    await axios.post(aisensy_api, data, { headers: headers })
     .then(response => {
         console.log('New Quote Notification Sent:', response.data);
       })
