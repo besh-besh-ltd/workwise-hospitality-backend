1369165d4c51b40937aac764812a68b7c9345028
ayush-workwise <ayush@letsworkwise.com>
2025-12-02 17:53:59 +0530

    feat: Add public user management endpoints and enhance public model functions

--- app/routes/public/publicRoutes.js
+++ app/routes/public/publicRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getProducts, getVendors } from "../../controllers/public/publicController.js";
+import { addPublicUsers, getProducts, getPublicUsers, getVendors } from "../../controllers/public/publicController.js";
 
 const PublicRoutes = Router();
 
 PublicRoutes.get('/products', getProducts);
 PublicRoutes.get('/vendors', getVendors);
+PublicRoutes.post('/add-public-users',addPublicUsers);
+PublicRoutes.get('/get-public-users',getPublicUsers);
 
 export default PublicRoutes;
